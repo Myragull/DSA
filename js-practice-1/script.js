2f91ed1d19923407cdbf638ded0eda2f42ca95bd
Myra Gull <myragull749@gmail.com>
2025-07-20 11:21:28 +0500

    Added the js-practice-1

---
+++ js-practice-1/script.js
@@ -0,0 +1,12 @@
+// write a program to swap 2 values
+
+// here i will be using the example of 2(glass1,glass2) empty glasses that 2 are filled and takes another empty one (glass3)
+// filled glass3 with glass1 now glass1 is empty
+// now filled glass1 with glass2 
+// now glass2 is empty filled it with glass3 now both of the vlaues are swapped using a third empty glass
+let a=30;
+let b=20;
+let c=a;
+a=b;
+b=c;
+console.log(a,b);
