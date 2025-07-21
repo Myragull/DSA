7cc46c5b5bb0aa36f912f934989f2098162f4d81
Myra Gull <myragull749@gmail.com>
2025-07-21 06:38:49 +0500

    Solve a total of 5 qs for for loops

--- js-practice-2/script.js
+++ js-practice-2/script.js
@@ -5,4 +5,43 @@
 // arr.forEach(function(value){
 //     console.log(value+2)
 // })
+ 
+// when the number of iterrations is known okay
+// for loop
+                                    //   1
+// let result =0;
+// for(let i=0 ; i<=6; i++){
+//      result=result+i;
+// }
+// console.log(result);
+                                // 2
+// for (let i=1; i<=10; i++){
+// if(i%2==0){
+//     console.log(i);
+// }
+// }
+                                //  3
+// Write a program using a for loop to calculate the factorial of a given number 10.
+// let factorial=1;
+// for(let i=1; i<=10; i++){
+// factorial = factorial*i;
+// }
+// console.log(factorial);
 
+                            //  4
+//  Write a program using a for loop that prints numbers from 10 down to 1.
+// for (let i=10 ; i>=1; i--){
+//     console.log(i);
+// }
+
+                        //    5
+// Write a program using a for loop to calculate and print the sum of all odd numbers from 1 to 7.
+
+// let sum = 0;
+// for (let i=0; i<=7; i++){
+//     if(i%2 !== 0)
+//     {
+//         sum = sum+i;
+//     }
+// }
+// console.log(sum);
