fb485c073bb2e7588be12d666c0793bda61a572a
Myra Gull <myragull749@gmail.com>
2025-07-21 05:40:42 +0500

    Create js-practice-2 file

---
+++ js-practice-2/script.js
@@ -0,0 +1,8 @@
+// forEach
+// it does something for each value and does not return a new array etc
+
+// let arr = [1,2,3,4];
+// arr.forEach(function(value){
+//     console.log(value+2)
+// })
+
---
+++ js-practice-2/tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+it does something for each value and does not return a new array etc
